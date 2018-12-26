a870ea68c5b443bec17c414bd6705df40cca3cbb
Sherdon Brown <sherdonbrowns88@gmail.com>
2018-12-26 18:19:13 -0500

    Initial commit

---
+++ app/src/test/java/firstproject2/mcs/com/androidcalculator/MainActivityTest.java
@@ -0,0 +1,28 @@
+package firstproject2.mcs.com.androidcalculator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainActivityTest {
+
+    @Test
+    public void onCreate() {
+    }
+
+    @Test
+    public void onAdd() {
+    }
+
+    @Test
+    public void onSub() {
+    }
+
+    @Test
+    public void onDiv() {
+    }
+
+    @Test
+    public void onMul() {
+    }
+}
